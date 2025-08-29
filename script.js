d71ae968bf9bad9b8d2d446a7fa07b2c901801d3
Shafawra <shafaaura35@gmail.com>
2025-08-29 15:21:49 +0800

    memodifikasi html dan js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const toggleMenu = document.querySelector(".toggleMenu");
 const sidebar = document.querySelector(".sidebar");
-const closeSidebar = document.querySelector(".closeSidebar");
 
-const menuIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-680v-80h720v80H120Zm0 480v-80h720v80H120Zm0-240v-80h720v80H120Z"/></svg>`;
+const menuIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="M120-680v-80h720v80H120Zm0 480v-80h720v80H120Zm0-240v-80h720v80H120Z"/></svg>`;
 
-const closeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
+const closeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
 
 toggleMenu.innerHTML = menuIcon;
 
@@ -12,7 +11,10 @@ let isOpen = false;
 
 toggleMenu.addEventListener("click", () => {
     isOpen = !isOpen;
-    toggleMenu.innerHTML = isOpen ? closeIcon : menuIcon;
-});
 
+    sidebar.classList.toggle("translate-x-0", isOpen);
+    sidebar.classList.toggle("-translate-x-full", !isOpen);
+
+    toggleMenu.innerHTML = isOpen ? closeIcon : menuIcon;
+})
 
