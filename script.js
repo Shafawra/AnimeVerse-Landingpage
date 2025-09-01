59174d517ca9e85b16ec0e7430aba8c31bc460e5
Shafawra <shafaaura35@gmail.com>
2025-09-01 14:43:17 +0800

    menghapus sidebar dan menambakan toggle menu khusus mobile

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-const toggleMenu = document.querySelector(".toggleMenu");
-const sidebar = document.querySelector(".sidebar");
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
 
-const menuIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="M120-680v-80h720v80H120Zm0 480v-80h720v80H120Zm0-240v-80h720v80H120Z"/></svg>`;
-
-const closeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
-
-toggleMenu.innerHTML = menuIcon;
-
-let isOpen = false;
-toggleMenu.innerHTML = menuIcon;
-
-toggleMenu.addEventListener("click", () => {
-    isOpen = !isOpen;
-
-    sidebar.classList.toggle("translate-x-0", isOpen);
-    sidebar.classList.toggle("-translate-x-full", !isOpen);
-
-    toggleMenu.innerHTML = isOpen ? closeIcon : menuIcon;
-})
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
 
 const cardCarousel = document.getElementById("cardCarousel");
 let arahScroll = 1;
